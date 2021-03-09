0fbd2b7295087b249a18e626723cb8d03884ad4e
jean cadet <cadetjean2001@gmail.com>
2021-03-09 14:53:32 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace simpleCAlculatorException_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtoperand = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtCalculate = new System.Windows.Forms.Button();
+            this.txtClear = new System.Windows.Forms.Button();
+            this.txtClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Numer";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Operand";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Result";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(164, 96);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(100, 20);
+            this.txtBox1.TabIndex = 4;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(164, 129);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(100, 20);
+            this.txtBox2.TabIndex = 5;
+            // 
+            // txtoperand
+            // 
+            this.txtoperand.Location = new System.Drawing.Point(164, 169);
+            this.txtoperand.Name = "txtoperand";
+            this.txtoperand.Size = new System.Drawing.Size(100, 20);
+            this.txtoperand.TabIndex = 6;
+            this.txtoperand.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(164, 216);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 7;
+            // 
+            // txtCalculate
+            // 
+            this.txtCalculate.Location = new System.Drawing.Point(316, 96);
+            this.txtCalculate.Name = "txtCalculate";
+            this.txtCalculate.Size = new System.Drawing.Size(75, 23);
+            this.txtCalculate.TabIndex = 8;
+            this.txtCalculate.Text = " Calculate";
+            this.txtCalculate.UseVisualStyleBackColor = true;
+            this.txtCalculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtClear
+            // 
+            this.txtClear.Location = new System.Drawing.Point(189, 285);
+            this.txtClear.Name = "txtClear";
+            this.txtClear.Size = new System.Drawing.Size(75, 23);
+            this.txtClear.TabIndex = 9;
+            this.txtClear.Text = "Clear";
+            this.txtClear.UseVisualStyleBackColor = true;
+            this.txtClear.Click += new System.EventHandler(this.txtClear_Click);
+            // 
+            // txtClose
+            // 
+            this.txtClose.Location = new System.Drawing.Point(59, 285);
+            this.txtClose.Name = "txtClose";
+            this.txtClose.Size = new System.Drawing.Size(75, 23);
+            this.txtClose.TabIndex = 10;
+            this.txtClose.Text = "Close";
+            this.txtClose.UseVisualStyleBackColor = true;
+            this.txtClose.Click += new System.EventHandler(this.txtClose_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.txtCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.txtClose;
+            this.ClientSize = new System.Drawing.Size(445, 450);
+            this.Controls.Add(this.txtClose);
+            this.Controls.Add(this.txtClear);
+            this.Controls.Add(this.txtCalculate);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtoperand);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtoperand;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button txtCalculate;
+        private System.Windows.Forms.Button txtClear;
+        private System.Windows.Forms.Button txtClose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿/*
+ *Hello Professor. I made one program that contain everything that was asked in the 2 excercise in one program Hope that is not a problem
+ *Since 6.1, 7.1 and 7.2 were all the same i integrated a catch FormatException, an OverflowException, and a DivideByZeroException. 
+ * i also created a method called  IsOperator that if the correct operator was use, and a IsValidData method that check if the user input is within the range that was given
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace simpleCAlculatorException_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {            decimal numOne = Convert.ToInt32(txtBox1.Text);
+
+            decimal numTwo = Convert.ToInt32(txtBox2.Text);
+            char operand = Convert.ToChar(txtoperand.Text);
+            decimal  result = 0;
+      calculate( numOne,numTwo,  operand, result);
+            }
+            //this catch format exception
+            catch (FormatException ex)
+            {
+                DialogResult dialogResult = MessageBox.Show(ex.Message);
+                txtResult.Text = Convert.ToString(dialogResult);
+            }
+            catch (System.OverflowException ex) {
+              DialogResult dialogResult = MessageBox.Show(ex.Message);
+               txtResult.Text = Convert.ToString(dialogResult);
+           }
+
+        }
+
+        //this call the calculate method and provide it with the user input
+        private void calculate( decimal numOne, decimal numTwo, char operand, decimal result) 
+        { 
+            //this also check the operator the user enter
+            try {
+
+                    isOperator(numOne,numTwo,operand,result);
+
+            }
+            //this catch deivide by zero exception
+            catch (System.DivideByZeroException ex)
+            {
+                DialogResult dialogResult = MessageBox.Show(ex.Message);
+                txtResult.Text = Convert.ToString(dialogResult); }
+
+
+
+        }
+
+
+        //this check if the correct operator is use than call the other method (isValiddata) to chack if the input is within the range
+        private void isOperator(decimal numOne, decimal numTwo, char operand, decimal result)
+        {
+            IsValidData(numOne, numTwo, operand, result);
+        }
+       private void  IsValidData (decimal numOne, decimal numTwo, char operand, decimal result)
+        {
+                if (numOne >= 0 && numOne <= 1000000 || numTwo >= 0 && numTwo <= 1000000)
+                {
+            if (operand == '+')
+            {
+                result = numOne + numTwo;
+                txtResult.Text = Convert.ToString(result);
+            }
+
+            else if (operand == '-')
+
+            {
+                result = numOne - numTwo;
+                txtResult.Text = Convert.ToString(result);
+            }
+
+            else if (operand == '/')
+
+            {
+                result = numOne / numTwo;
+                txtResult.Text = Convert.ToString(result);
+            }
+
+            else if (operand == '*')
+
+            {
+                result = numOne * numTwo;
+                txtResult.Text = Convert.ToString(result);
+            }
+            else { txtResult.Text = "invalid Operand"; }
+                }
+
+                else { txtResult.Text = "out of range"; }
+            }
+
+        //clear the form 
+        private void txtClear_Click(object sender, EventArgs e)
+        {
+           txtResult.Text= String.Empty;
+            txtBox1.Text = String.Empty;
+            txtBox2.Text = String.Empty;
+            txtoperand.Text= String.Empty;
+        }
+
+        //close the form
+        private void txtClose_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
